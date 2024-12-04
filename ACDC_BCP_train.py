9b9f1abd078386becca9dc8f5110e719ea4a4ea3
tokyo <823818639@qq.com>
2024-12-04 15:59:16 +0800

    code

---
+++ ACDC_BCP_train.py
@@ -0,0 +1,449 @@
+import argparse
+import logging
+import os
+import random
+import shutil
+import sys
+
+import numpy as np
+import torch
+import torch.backends.cudnn as cudnn
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from tensorboardX import SummaryWriter
+from torch.utils.data import DataLoader
+from torch.nn.modules.loss import CrossEntropyLoss
+from torchvision import transforms
+from tqdm import tqdm
+from skimage.measure import label
+
+from dataloaders.dataset import (BaseDataSets, RandomGenerator, TwoStreamBatchSampler, ThreeStreamBatchSampler)
+from networks.net_factory import BCP_net, net_factory
+from utils import losses, ramps, feature_memory, contrastive_losses, val_2d
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--root_path', type=str, default='../data_split/ACDC', help='Name of Experiment')
+parser.add_argument('--exp', type=str, default='BCP', help='experiment_name')
+parser.add_argument('--model', type=str, default='unet', help='model_name')
+parser.add_argument('--pre_iterations', type=int, default=20000, help='maximum epoch number to train')
+parser.add_argument('--max_iterations', type=int, default=60000, help='maximum epoch number to train')
+parser.add_argument('--batch_size', type=int, default=24, help='batch_size per gpu')
+parser.add_argument('--deterministic', type=int, default=1, help='whether use deterministic training')
+parser.add_argument('--base_lr', type=float, default=0.01, help='segmentation network learning rate')
+parser.add_argument('--patch_size', type=list, default=[256, 256], help='patch size of network input')
+parser.add_argument('--seed', type=int, default=1337, help='random seed')
+parser.add_argument('--num_classes', type=int, default=4, help='output channel of network')
+# label and unlabel
+parser.add_argument('--labeled_bs', type=int, default=12, help='labeled_batch_size per gpu')
+parser.add_argument('--labelnum', type=int, default=14, help='labeled data')
+parser.add_argument('--u_weight', type=float, default=0.5, help='weight of unlabeled pixels')
+# costs
+parser.add_argument('--gpu', type=str, default='0', help='GPU to use')
+parser.add_argument('--consistency', type=float, default=0.1, help='consistency')
+parser.add_argument('--consistency_rampup', type=float, default=200.0, help='consistency_rampup')
+parser.add_argument('--magnitude', type=float, default='6.0', help='magnitude')
+parser.add_argument('--s_param', type=int, default=6, help='multinum of random masks')
+
+args = parser.parse_args()
+
+dice_loss = losses.DiceLoss(n_classes=4)
+
+if args.deterministic:
+    cudnn.benchmark = False
+    cudnn.deterministic = True
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    torch.cuda.manual_seed(args.seed)
+
+def load_net(net, path):
+    state = torch.load(str(path))
+    net.load_state_dict(state['net'])
+
+
+def load_net_opt(net, optimizer, path):
+    state = torch.load(str(path))
+    net.load_state_dict(state['net'])
+    optimizer.load_state_dict(state['opt'])
+
+
+def save_net_opt(net, optimizer, path):
+    state = {
+        'net': net.state_dict(),
+        'opt': optimizer.state_dict(),
+    }
+    torch.save(state, str(path))
+
+
+def get_ACDC_LargestCC(segmentation):
+    class_list = []
+    for i in range(1, 4):
+        temp_prob = segmentation == i * torch.ones_like(segmentation)
+        temp_prob = temp_prob.detach().cpu().numpy()
+        labels = label(temp_prob)
+        # -- with 'try'
+        assert (labels.max() != 0)  # assume at least 1 CC
+        largestCC = labels == np.argmax(np.bincount(labels.flat)[1:]) + 1
+        class_list.append(largestCC * i)
+    acdc_largestCC = class_list[0] + class_list[1] + class_list[2]
+    return torch.from_numpy(acdc_largestCC).cuda()
+
+
+def get_ACDC_2DLargestCC(segmentation):
+    batch_list = []
+    N = segmentation.shape[0]
+    for i in range(0, N):
+        class_list = []
+        for c in range(1, 4):
+            temp_seg = segmentation[i]  # == c *  torch.ones_like(segmentation[i])
+            temp_prob = torch.zeros_like(temp_seg)
+            temp_prob[temp_seg == c] = 1
+            temp_prob = temp_prob.detach().cpu().numpy()
+            labels = label(temp_prob)
+            if labels.max() != 0:
+                largestCC = labels == np.argmax(np.bincount(labels.flat)[1:]) + 1
+                class_list.append(largestCC * c)
+            else:
+                class_list.append(temp_prob)
+
+        n_batch = class_list[0] + class_list[1] + class_list[2]
+        batch_list.append(n_batch)
+
+    return torch.Tensor(np.array(batch_list)).cuda()
+
+
+# def get_ACDC_masks(output, nms=0):
+#     probs = F.softmax(output, dim=1)
+#     _, probs = torch.max(probs, dim=1)
+#     if nms == 1:
+#         probs = get_ACDC_2DLargestCC(probs)
+#     return probs
+
+def get_ACDC_masks(output, thres, nms=0):
+    probs = F.softmax(output, dim=1)
+    probs = probs >= thres
+    _, probs = torch.max(probs, dim=1)
+    if nms == 1:
+        probs = get_ACDC_2DLargestCC(probs)
+    return probs
+
+# def get_ACDC_masks(output, thres, nms=0):
+#     probs = F.softmax(output, dim=1)
+#     _, labels = torch.max(probs, dim=1)
+#     masks = (probs >= thres)  # 使用阈值生成掩码
+#     labels = labels.float() * masks.float()  # 将标签乘以掩码
+#     if nms == 1:
+#         labels = get_ACDC_2DLargestCC(labels)
+#     return labels
+def update_model_ema(model, ema_model, alpha):
+    model_state = model.state_dict()
+    model_ema_state = ema_model.state_dict()
+    new_dict = {}
+    for key in model_state:
+        new_dict[key] = alpha * model_ema_state[key] + (1 - alpha) * model_state[key]
+    ema_model.load_state_dict(new_dict)
+
+
+def mix_loss(output_l, output_u, img_l, patch_l, l_weight=1.0, u_weight=0.5, unlab=False):
+    CE = nn.CrossEntropyLoss(reduction='none')
+    img_l, patch_l = img_l.type(torch.int64), patch_l.type(torch.int64)
+    output_soft_l = F.softmax(output_l, dim=1)
+    output_soft_u = F.softmax(output_u, dim=1)
+    image_weight, patch_weight = l_weight, u_weight
+    if unlab:
+        image_weight, patch_weight = u_weight, l_weight
+    loss_dice = dice_loss(output_soft_l, img_l.unsqueeze(1)) * image_weight
+    loss_dice += dice_loss(output_soft_u, patch_l.unsqueeze(1)) * patch_weight
+    loss_ce = image_weight * (F.cross_entropy(output_l, img_l))
+    loss_ce += patch_weight * (F.cross_entropy(output_u, patch_l))  # loss = loss_ce
+    return loss_dice, loss_ce
+
+
+def patients_to_slices(dataset, patiens_num):
+    ref_dict = None
+    if "ACDC" in dataset:
+        ref_dict = {"1": 32, "3": 68, "7": 136,
+                    "14": 256, "21": 396, "28": 512, "35": 664, "70": 1312}
+    elif "Prostate":
+        ref_dict = {"2": 27, "4": 53, "8": 120,
+                    "12": 179, "16": 256, "21": 312, "42": 623}
+    else:
+        print("Error")
+    return ref_dict[str(patiens_num)]
+
+
+def worker_init_fn(worker_id):
+    random.seed(args.seed + worker_id)
+
+
+def pre_train(args, snapshot_path):
+    CE = nn.CrossEntropyLoss(reduction='none')
+    DICE = losses.DiceLoss(n_classes=4)
+    base_lr = args.base_lr
+    num_classes = args.num_classes
+    max_iterations = args.pre_iterations
+    os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
+    pre_trained_model = os.path.join(pre_snapshot_path, '{}_best_model.pth'.format(args.model))
+    labeled_sub_bs, unlabeled_sub_bs = int(args.labeled_bs / 2), int((args.batch_size - args.labeled_bs) / 2)
+
+    model = BCP_net(in_chns=1, class_num=num_classes)
+
+    db_train = BaseDataSets(base_dir=args.root_path,
+                            split="train",
+                            num=None,
+                            transform=transforms.Compose([RandomGenerator(args.patch_size)]))
+    db_val = BaseDataSets(base_dir=args.root_path, split="val")
+    total_slices = len(db_train)
+    labeled_slice = patients_to_slices(args.root_path, args.labelnum)
+    print("Total slices is: {}, labeled slices is:{}".format(total_slices, labeled_slice))
+    labeled_idxs = list(range(0, labeled_slice))
+    unlabeled_idxs = list(range(labeled_slice, total_slices))
+    batch_sampler = TwoStreamBatchSampler(labeled_idxs, unlabeled_idxs, args.batch_size,
+                                          args.batch_size - args.labeled_bs)
+
+    trainloader = DataLoader(db_train, batch_sampler=batch_sampler, num_workers=0, pin_memory=True,
+                             worker_init_fn=worker_init_fn)
+
+    valloader = DataLoader(db_val, batch_size=1, shuffle=False, num_workers=1)
+
+    optimizer = optim.SGD(model.parameters(), lr=base_lr, momentum=0.9, weight_decay=0.0001)
+
+    writer = SummaryWriter(snapshot_path + '/log')
+    logging.info("Start pre_training")
+    logging.info("{} iterations per epoch".format(len(trainloader)))
+
+    model.train()
+
+    iter_num = 0
+    max_epoch = max_iterations // len(trainloader) + 1
+    best_performance = 0.0
+    best_hd = 100
+    iterator = tqdm(range(max_epoch), ncols=70)
+    for _ in iterator:
+        for _, sampled_batch in enumerate(trainloader):
+            volume_batch, label_batch = sampled_batch['image'][:args.labeled_bs], sampled_batch['label'][
+                                                                                  :args.labeled_bs]
+            volume_batch, label_batch = volume_batch.cuda(), label_batch.cuda()
+
+            # -- original
+            output = model(volume_batch)
+
+            label_batch = label_batch.type(torch.int64)
+            output_soft_l = F.softmax(output, dim=1)
+            loss_dice = DICE(output_soft_l, label_batch.unsqueeze(1))
+            loss_ce = F.cross_entropy(output, label_batch)
+            loss = (loss_dice + loss_ce) / 2
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            iter_num += 1
+
+            writer.add_scalar('info/total_loss', loss, iter_num)
+            writer.add_scalar('info/mix_dice', loss_dice, iter_num)
+            writer.add_scalar('info/mix_ce', loss_ce, iter_num)
+
+            logging.info('iteration %d: loss: %f, mix_dice: %f, mix_ce: %f' % (iter_num, loss, loss_dice, loss_ce))
+
+            if iter_num > 0 and iter_num % 100 == 0:
+                model.eval()
+                metric_list = 0.0
+                for _, sampled_batch in enumerate(valloader):
+                    metric_i = val_2d.test_single_volume(sampled_batch["image"], sampled_batch["label"], model,
+                                                         classes=num_classes)
+                    metric_list += np.array(metric_i)
+                metric_list = metric_list / len(db_val)
+                for class_i in range(num_classes - 1):
+                    writer.add_scalar('info/val_{}_dice'.format(class_i + 1), metric_list[class_i, 0], iter_num)
+                    writer.add_scalar('info/val_{}_hd95'.format(class_i + 1), metric_list[class_i, 1], iter_num)
+
+                performance = np.mean(metric_list, axis=0)[0]
+                writer.add_scalar('info/val_mean_dice', performance, iter_num)
+
+                if performance > best_performance:
+                    best_performance = performance
+                    save_mode_path = os.path.join(snapshot_path,
+                                                  'iter_{}_dice_{}.pth'.format(iter_num, round(best_performance, 4)))
+                    save_best_path = os.path.join(snapshot_path, '{}_best_model.pth'.format(args.model))
+                    save_net_opt(model, optimizer, save_mode_path)
+                    save_net_opt(model, optimizer, save_best_path)
+
+                logging.info('iteration %d : mean_dice : %f' % (iter_num, performance))
+                model.train()
+
+            if iter_num >= max_iterations:
+                break
+        if iter_num >= max_iterations:
+            iterator.close()
+            break
+    writer.close()
+
+
+def self_train(args, pre_snapshot_path, snapshot_path):
+    base_lr = args.base_lr
+    num_classes = args.num_classes
+    max_iterations = args.max_iterations
+    os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
+    pre_trained_model = os.path.join(pre_snapshot_path, '{}_best_model.pth'.format(args.model))
+    labeled_sub_bs, unlabeled_sub_bs = int(args.labeled_bs / 2), int((args.batch_size - args.labeled_bs) / 2)
+    mse = nn.MSELoss()
+    model = BCP_net(in_chns=1, class_num=num_classes)
+    ema_model = BCP_net(in_chns=1, class_num=num_classes, ema=True)
+    DICE = losses.DiceLoss(n_classes=4)
+    db_train = BaseDataSets(base_dir=args.root_path,
+                            split="train",
+                            num=None,
+                            transform=transforms.Compose([RandomGenerator(args.patch_size)]))
+    db_val = BaseDataSets(base_dir=args.root_path, split="val")
+    total_slices = len(db_train)
+    labeled_slice = patients_to_slices(args.root_path, args.labelnum)
+    print("Total slices is: {}, labeled slices is:{}".format(total_slices, labeled_slice))
+    labeled_idxs = list(range(0, labeled_slice))
+    unlabeled_idxs = list(range(labeled_slice, total_slices))
+    batch_sampler = TwoStreamBatchSampler(labeled_idxs, unlabeled_idxs, args.batch_size,
+                                          args.batch_size - args.labeled_bs)
+
+    trainloader = DataLoader(db_train, batch_sampler=batch_sampler, num_workers=0, pin_memory=True,
+                             worker_init_fn=worker_init_fn)
+
+    valloader = DataLoader(db_val, batch_size=1, shuffle=False, num_workers=1)
+
+    optimizer = optim.SGD(model.parameters(), lr=base_lr, momentum=0.9, weight_decay=0.0001)
+    load_net(ema_model, pre_trained_model)
+    load_net_opt(model, optimizer, pre_trained_model)
+    logging.info("Loaded from {}".format(pre_trained_model))
+
+    writer = SummaryWriter(snapshot_path + '/log')
+    logging.info("Start self_training")
+    logging.info("{} iterations per epoch".format(len(trainloader)))
+
+    model.train()
+    ema_model.train()
+
+    ce_loss = CrossEntropyLoss()
+
+    iter_num = 0
+    max_epoch = max_iterations // len(trainloader) + 1
+    best_performance = 0.0
+    best_hd = 100
+    iterator = tqdm(range(max_epoch), ncols=70)
+    for _ in iterator:
+        for _, sampled_batch in enumerate(trainloader):
+            labeled_volume, labeled_label = sampled_batch['image'][:args.labeled_bs], sampled_batch['label'][
+                                                                                      :args.labeled_bs]
+            labeled_volume, labeled_label = labeled_volume.cuda(), labeled_label.cuda()
+
+            unlabeled_volume = sampled_batch['image'][args.labeled_bs:]
+            unlabeled_volume = unlabeled_volume.cuda()
+
+            outputs_u = model(unlabeled_volume)
+            outputs_l = model(labeled_volume)
+
+            with torch.no_grad():
+                ema_output = ema_model(unlabeled_volume)
+                ema_output_l = ema_model(labeled_volume)
+
+            labeled_label_vs = labeled_label.type(torch.int64)
+            output_soft_l = F.softmax(outputs_l, dim=1)
+            ema_output_soft_l = F.softmax(ema_output_l, dim=1)
+
+            S_sup = DICE(output_soft_l, labeled_label_vs.unsqueeze(1))
+            T_sup = DICE(ema_output_soft_l, labeled_label_vs.unsqueeze(1))
+
+            if T_sup < S_sup:
+                print("Teacher win")
+                thres_t = 0.5
+                thres = 0.5
+                dice = 10
+                with torch.no_grad():
+                    for i in range(5):
+                        pseudo_label = get_ACDC_masks(ema_output_l, thres, nms=1)
+                        dice_t = mse(pseudo_label, labeled_label)
+                        if dice_t < dice:
+                            dice = dice_t
+                            thres_t = thres
+                        thres += 0.1
+                    print(thres_t)
+                    pseudo_label = get_ACDC_masks(ema_output, thres_t, nms=1)
+                loss_dice, loss_ce = mix_loss(outputs_l, outputs_u, labeled_label, pseudo_label,
+                                              u_weight=1.0, unlab=True)
+            else:
+                print("Student win")
+                loss_dice = S_sup
+                labeled_label = labeled_label.type(torch.int64)
+                loss_ce = F.cross_entropy(outputs_l, labeled_label)
+
+            loss = (loss_dice + loss_ce) / 2
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            iter_num += 1
+            update_model_ema(model, ema_model, 0.999)
+
+            writer.add_scalar('info/total_loss', loss, iter_num)
+            writer.add_scalar('info/mix_dice', loss_dice, iter_num)
+            writer.add_scalar('info/mix_ce', loss_ce, iter_num)
+
+            logging.info('iteration %d: loss: %f, mix_dice: %f, mix_ce: %f' % (iter_num, loss, loss_dice, loss_ce))
+
+            if iter_num > 0 and iter_num % 100 == 0:
+                model.eval()
+                metric_list = 0.0
+                for _, sampled_batch in enumerate(valloader):
+                    metric_i = val_2d.test_single_volume(sampled_batch["image"], sampled_batch["label"], model,
+                                                         classes=num_classes)
+                    metric_list += np.array(metric_i)
+                metric_list = metric_list / len(db_val)
+                for class_i in range(num_classes - 1):
+                    writer.add_scalar('info/val_{}_dice'.format(class_i + 1), metric_list[class_i, 0], iter_num)
+                    writer.add_scalar('info/val_{}_hd95'.format(class_i + 1), metric_list[class_i, 1], iter_num)
+
+                performance = np.mean(metric_list, axis=0)[0]
+                writer.add_scalar('info/val_mean_dice', performance, iter_num)
+
+                if performance > best_performance:
+                    best_performance = performance
+                    save_mode_path = os.path.join(snapshot_path,
+                                                  'iter_{}_dice_{}.pth'.format(iter_num, round(best_performance, 4)))
+                    save_best_path = os.path.join(snapshot_path, '{}_best_model.pth'.format(args.model))
+                    torch.save(model.state_dict(), save_mode_path)
+                    torch.save(model.state_dict(), save_best_path)
+
+                logging.info('iteration %d : mean_dice : %f' % (iter_num, performance))
+                model.train()
+
+            if iter_num >= max_iterations:
+                break
+        if iter_num >= max_iterations:
+            iterator.close()
+            break
+    writer.close()
+
+
+if __name__ == "__main__":
+
+    # -- path to save models
+    pre_snapshot_path = "./model/BCP/ACDC_{}_{}_labeled/pre_train".format(args.exp, args.labelnum)
+    self_snapshot_path = "./model/BCP/ACDC_{}_{}_labeled/self_train".format(args.exp, args.labelnum)
+    for snapshot_path in [pre_snapshot_path, self_snapshot_path]:
+        if not os.path.exists(snapshot_path):
+            os.makedirs(snapshot_path)
+    shutil.copy('../code/ACDC_BCP_train.py', self_snapshot_path)
+
+    # Pre_train
+    logging.basicConfig(filename=pre_snapshot_path + "/log.txt", level=logging.INFO,
+                        format='[%(asctime)s.%(msecs)03d] %(message)s', datefmt='%H:%M:%S')
+    logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+    logging.info(str(args))
+    pre_train(args, pre_snapshot_path)
+
+    # Self_train
+    logging.basicConfig(filename=self_snapshot_path + "/log.txt", level=logging.INFO,
+                        format='[%(asctime)s.%(msecs)03d] %(message)s', datefmt='%H:%M:%S')
+    logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+    logging.info(str(args))
+    self_train(args, pre_snapshot_path, self_snapshot_path)
